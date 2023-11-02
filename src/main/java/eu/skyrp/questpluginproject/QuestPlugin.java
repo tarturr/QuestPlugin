6adfc75415ef9b49facb1bfc2ff075d22f2be03a
tarturr <tartur.dev@gmail.com>
2023-11-03 00:20:18 +0100

    Added tests of the new plugin classes

--- src/main/java/eu/skyrp/questpluginproject/QuestPlugin.java
+++ src/main/java/eu/skyrp/questpluginproject/QuestPlugin.java
@@ -1,17 +1,40 @@
 package eu.skyrp.questpluginproject;
 
+import eu.skyrp.questpluginproject.lib.database.connection.DatabaseConnectionManager;
+import eu.skyrp.questpluginproject.quest.Quest;
+import eu.skyrp.questpluginproject.quest.QuestReward;
+import eu.skyrp.questpluginproject.quest.QuestType;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class QuestPlugin extends JavaPlugin {
 
+    private DatabaseConnectionManager connections;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        saveDefaultConfig();
+        this.connections = new DatabaseConnectionManager(this, "databases");
 
+        Quest test = Quest.builder()
+                .id("test")
+                .type(QuestType.ONLY_ONCE)
+                .name("Quête de test")
+                .descriptionLine("Ceci est une")
+                .descriptionLine("quête de test.")
+                .reward(QuestReward.builder()
+                        .item("testitem", 1)
+                        .experience(250.0f)
+                        .money(10.0f)
+                        .command("/hello")
+                        .build())
+                .build();
+
+        this.connections.findDatabase("quests").ifPresent(test::createInDatabase);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        this.connections.closeAll();
     }
+
 }
