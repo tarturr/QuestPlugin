d4d56b8c9ceab2f00a257e8f7244f03e77988404
tarturr <tartur.dev@gmail.com>
2023-11-28 00:15:13 +0100

    Marked BaseMechanic abstract class as an observable class

--- src/main/java/eu/skyrp/questpluginproject/quest/common/mechanic/BaseMechanic.java
+++ src/main/java/eu/skyrp/questpluginproject/quest/common/mechanic/BaseMechanic.java
@@ -3,25 +3,52 @@ package eu.skyrp.questpluginproject.quest.common.mechanic;
 import eu.skyrp.questpluginproject.quest.common.objective.BaseQuestObjective;
 import lombok.Getter;
 import lombok.Singular;
+import lombok.experimental.Accessors;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.util.List;
 import java.util.UUID;
 
-public abstract class BaseMechanic<T extends BaseQuestObjective<?, ?>> {
+@Accessors(fluent = true)
+public abstract class BaseMechanic<T extends BaseQuestObjective<?, ?>> implements PropertyChangeListener {
 
-    private final JavaPlugin main;
     @Getter
     private final UUID playerUUID;
     @Getter
     @Singular("objective")
     private final List<T> objectives;
+    private int endedObjectives;
+
+    private final PropertyChangeSupport mechanicEndSupport;
 
     public BaseMechanic(JavaPlugin main, UUID playerUUID, List<T> objectives) {
-        this.main = main;
         this.playerUUID = playerUUID;
         this.objectives = objectives;
-        this.objectives.forEach(objective -> main.getServer().getPluginManager().registerEvents(objective, main));
+        this.endedObjectives = 0;
+
+        this.objectives.forEach(objective -> {
+            objective.endQuestSupport().addPropertyChangeListener(this);
+            main.getServer().getPluginManager().registerEvents(objective, main);
+        });
+
+        this.mechanicEndSupport = new PropertyChangeSupport(this);
     }
 
+    @Override
+    public void propertyChange(PropertyChangeEvent event) {
+        if (event.getPropertyName().equals("hasEnded")) {
+            this.endedObjectives++;
+
+            if (this.endedObjectives == this.objectives.size()) {
+                this.mechanicEndSupport.firePropertyChange("objectivesAllEnded", false, true);
+            }
+        }
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        this.mechanicEndSupport.addPropertyChangeListener(listener);
+    }
 }
