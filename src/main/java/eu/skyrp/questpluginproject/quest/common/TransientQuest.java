da3fde7c30d68557dff0c9c8690f0b46b6c5dce1
tarturr <tartur.dev@gmail.com>
2023-11-28 00:21:16 +0100

    Created a type of Quest which has an end date.

---
+++ src/main/java/eu/skyrp/questpluginproject/quest/common/TransientQuest.java
@@ -0,0 +1,34 @@
+package eu.skyrp.questpluginproject.quest.common;
+
+import eu.skyrp.questpluginproject.quest.common.mechanic.BaseMechanic;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+import org.bukkit.entity.Player;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Accessors(fluent = true)
+public abstract class TransientQuest extends Quest {
+
+    private final Date end;
+
+    public TransientQuest(String id, String name, List<String> lore, QuestReward reward, List<BaseMechanic<?>> mechanics, int durationInDays) {
+        super(id, name, lore, reward, mechanics);
+        this.end = new Date(System.currentTimeMillis() + (long) durationInDays * 24 * 3600 * 1000);
+    }
+
+    public void reload(Player player) {
+        if (this.getTimeLeft() < 0 && super.state() == QuestState.PENDING) {
+            this.forceQuestEnd(player);
+        }
+    }
+
+    public abstract void forceQuestEnd(Player player);
+
+    public long getTimeLeft() {
+        return this.end.getTime() - System.currentTimeMillis();
+    }
+
+}
