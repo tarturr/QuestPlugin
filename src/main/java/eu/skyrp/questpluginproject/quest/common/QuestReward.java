27a06a3183741334a1980d93d7424d390d3c9490
tarturr <tartur.dev@gmail.com>
2023-11-28 18:01:29 +0100

    Added @Builder lombok annotation for QuestReward

--- src/main/java/eu/skyrp/questpluginproject/quest/common/QuestReward.java
+++ src/main/java/eu/skyrp/questpluginproject/quest/common/QuestReward.java
@@ -1,13 +1,12 @@
 package eu.skyrp.questpluginproject.quest.common;
 
 import lombok.Builder;
-import lombok.Getter;
 import lombok.Singular;
 import lombok.experimental.Accessors;
 
 import java.util.*;
 
-@Getter
 @Accessors(fluent = true)
+@Builder
 public record QuestReward(@Singular("item") Map<String, Integer> items, float experience, float money, @Singular("command") List<String> commands) {
 }
