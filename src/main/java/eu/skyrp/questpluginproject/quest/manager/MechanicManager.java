661d9fe03c969b11bc4b1721d9a188f38a6fbe29
tarturr <tartur.dev@gmail.com>
2023-12-17 19:32:18 +0100

    Set MechanicManager as an observer of the BaseMechanic class

--- src/main/java/eu/skyrp/questpluginproject/quest/manager/MechanicManager.java
+++ src/main/java/eu/skyrp/questpluginproject/quest/manager/MechanicManager.java
@@ -2,20 +2,31 @@ package eu.skyrp.questpluginproject.quest.manager;
 
 import eu.skyrp.questpluginproject.quest.common.mechanic.BaseMechanic;
 import eu.skyrp.questpluginproject.quest.common.mechanic.MechanicType;
+import lombok.Getter;
+import lombok.experimental.Accessors;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.plugin.java.JavaPlugin;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.util.*;
 
-public class MechanicManager {
+public class MechanicManager implements PropertyChangeListener {
 
+    @Getter
+    @Accessors(fluent = true)
     private final List<BaseMechanic<?>> mechanics;
+    private int endedMechanics;
+
+    private final PropertyChangeSupport support;
 
     public MechanicManager(List<BaseMechanic<?>> mechanics) {
         this.mechanics = mechanics;
-    }
+        this.endedMechanics = 0;
+        this.support = new PropertyChangeSupport(this);
 
-    public List<BaseMechanic<?>> getAllMechanics() {
-        return this.mechanics;
+        this.mechanics.forEach(mechanic -> mechanic.addPropertyChangeListener(this));
     }
 
     public static MechanicManager createFromConfigurationSection(String name, ConfigurationSection mechanicsSection) {
@@ -25,11 +36,30 @@ public class MechanicManager {
             ConfigurationSection currentMechanicSection = Objects.requireNonNull(mechanicsSection.getConfigurationSection(mechanicName));
 
             Arrays.stream(MechanicType.valueOf(mechanicName.toUpperCase()).associatedMechanics()
-                    .apply(name, currentMechanicSection)).filter(possibleMechanic -> !possibleMechanic.objectives().isEmpty())
+                    .apply(name, currentMechanicSection))
+                    .filter(possibleMechanic -> !possibleMechanic.objectives().isEmpty())
                     .forEach(mechanics::add);
         }
 
         return new MechanicManager(mechanics);
     }
 
+    @Override
+    public void propertyChange(PropertyChangeEvent event) {
+        if (event.getPropertyName().equals("mechanicEnded")) {
+            this.endedMechanics++;
+
+            if (this.endedMechanics == this.mechanics.size()) {
+                this.support.firePropertyChange("allMechanicsEnded", false, true);
+            }
+        }
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        this.support.addPropertyChangeListener(listener);
+    }
+
+    public void registerAllMechanics(JavaPlugin main) {
+        this.mechanics.forEach(mechanic -> mechanic.registerAllObjectives(main));
+    }
 }
