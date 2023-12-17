2e3a722a77c04246625c9c3df8a5cf2d9f3ee069
tarturr <tartur.dev@gmail.com>
2023-12-17 19:29:51 +0100

    Added a QuestManager class to handle all the existing quests

---
+++ src/main/java/eu/skyrp/questpluginproject/quest/manager/QuestManager.java
@@ -0,0 +1,83 @@
+package eu.skyrp.questpluginproject.quest.manager;
+
+import eu.skyrp.questpluginproject.loader.QuestLoader;
+import eu.skyrp.questpluginproject.quest.common.PlayerQuests;
+import eu.skyrp.questpluginproject.quest.common.Quest;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.File;
+import java.util.*;
+
+public class QuestManager implements PropertyChangeListener {
+
+    private final List<PlayerQuests> playerQuests;
+    private QuestLoader questLoader;
+    private final File parentFolder;
+
+    private final JavaPlugin main;
+
+    public QuestManager(JavaPlugin main) {
+        this.playerQuests = new ArrayList<>();
+        this.parentFolder = main.getDataFolder();
+        this.questLoader = new QuestLoader(this.parentFolder);
+        this.main = main;
+    }
+
+    public PlayerQuests getPlayerQuests(UUID uuid) {
+        Optional<PlayerQuests> foundQuests = this.playerQuests.stream().filter(playerQuests -> playerQuests.uuid().equals(uuid)).findFirst();
+
+        if (foundQuests.isEmpty()) {
+            PlayerQuests playerQuests = new PlayerQuests(uuid);
+            this.playerQuests.add(playerQuests);
+            return playerQuests;
+        }
+
+        return foundQuests.get();
+    }
+
+    /**
+     * Ajoute une nouvelle PlayerQuest.
+     * @param playerUUID L'UUID du joueur.
+     * @param questId L'ID de la Quest.
+     * @return true si le joueur n'a pas déjà la quête et qu'elle a donc été ajoutée, false sinon.
+     */
+    public boolean addPlayerQuest(UUID playerUUID, String questId) {
+        return this.addPlayerQuest(this.getPlayerQuests(playerUUID), questId);
+    }
+
+    private boolean addPlayerQuest(PlayerQuests quests, String questId) {
+        Optional<Quest> searchedQuest = this.questLoader.getQuestById(questId);
+
+        if (searchedQuest.isPresent()) {
+            boolean result = quests.add(searchedQuest.get());
+            quests.addPropertyChangeListener(this);
+            quests.registerAllQuests(this.main);
+            return result;
+        }
+
+        throw new IllegalArgumentException("[QuestPlugin] The provided \"" + questId + "\" does not exist or is not " +
+                "loaded.");
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent event) {
+        if (event.getPropertyName().equals("nextQuest")) {
+            PlayerQuests quests = (PlayerQuests) event.getOldValue();
+            String nextQuestId = (String) event.getNewValue();
+
+            boolean success = this.addPlayerQuest(quests, nextQuestId);
+
+            if (!success) {
+                throw new IllegalStateException("[QuestPlugin] The required \"" + nextQuestId + "\" quest is not " +
+                        "loaded but is required as a next quest.");
+            }
+        }
+    }
+
+    public void reload() {
+        this.playerQuests.clear();
+        this.questLoader = new QuestLoader(this.parentFolder);
+    }
+}
