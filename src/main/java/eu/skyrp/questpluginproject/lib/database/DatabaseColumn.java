24fd9d094aba4c4a576410530a9ce7e2813dc57d
tarturr <tartur.dev@gmail.com>
2023-12-20 16:50:46 +0100

    Added some documentation to the DatabaseColumn classes.

--- src/main/java/eu/skyrp/questpluginproject/lib/database/DatabaseColumn.java
+++ src/main/java/eu/skyrp/questpluginproject/lib/database/DatabaseColumn.java
@@ -2,11 +2,41 @@ package eu.skyrp.questpluginproject.lib.database;
 
 import eu.skyrp.questpluginproject.lib.database.connection.BaseDatabaseConnection;
 
+/**
+ * Represents a column of a database table.
+ * @param <T> The type of the primary key.
+ */
 public interface DatabaseColumn<T> {
 
+    /**
+     * Creates a new column instance in the database via the provided database using the {@link BaseDatabaseConnection}
+     * class.
+     * @param connection The provided database connection.
+     * @return true if the creation was successful, false otherwise.
+     */
     boolean createInDatabase(BaseDatabaseConnection connection);
+
+    /**
+     * Checks if the current class exists in the provided database using the {@link BaseDatabaseConnection} class.
+     * @param connection The provided database connection.
+     * @return true if the check was successful, false otherwise.
+     */
     boolean existsInDatabase(BaseDatabaseConnection connection);
+
+    /**
+     * Updates the new values of the class fields in the provided database using the {@link BaseDatabaseConnection}
+     * class.
+     * @param connection The provided database connection.
+     * @return true if the update was successful, false otherwise.
+     */
     boolean update(BaseDatabaseConnection connection);
+
+    /**
+     * Fetches an instance of the T class from the provided database using the {@link BaseDatabaseConnection} class.
+     * @param primaryKey The value of the primary key.
+     * @param connection The provided database connection.
+     * @return true if the instance was successfully created, false otherwise.
+     */
     boolean fetchFromDatabase(T primaryKey, BaseDatabaseConnection connection);
 
 }
--- src/main/java/eu/skyrp/questpluginproject/lib/database/DatabaseColumnAutoIncrement.java
+++ src/main/java/eu/skyrp/questpluginproject/lib/database/DatabaseColumnAutoIncrement.java
@@ -2,7 +2,17 @@ package eu.skyrp.questpluginproject.lib.database;
 
 import eu.skyrp.questpluginproject.lib.database.connection.BaseDatabaseConnection;
 
-public interface DatabaseColumnAutoIncrement<T> extends DatabaseColumn<T> {
+/**
+ * Represents a column of a database table which has a auto increment column as its primary key.
+ * @see DatabaseColumn
+ */
+public interface DatabaseColumnAutoIncrement extends DatabaseColumn<Integer> {
+
+    /**
+     * Fetch the id of the current class from the provided database using the {@link BaseDatabaseConnection} class.
+     * @param connection The provided database connection.
+     * @return the id if it was found in the database, a negative number (preferably -1) otherwise.
+     */
     public abstract int fetchId(BaseDatabaseConnection connection);
 
 }
