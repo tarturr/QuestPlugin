6cce7488dae19b8295103771c57d9546e6a2830c
tarturr <tartur.dev@gmail.com>
2023-12-29 16:59:15 +0100

    Turned DatabaseColumnAutoIncrement interface to abstract class

--- src/main/java/eu/skyrp/questpluginproject/lib/database/DatabaseColumnAutoIncrement.java
+++ src/main/java/eu/skyrp/questpluginproject/lib/database/DatabaseColumnAutoIncrement.java
@@ -1,18 +1,63 @@
 package eu.skyrp.questpluginproject.lib.database;
 
 import eu.skyrp.questpluginproject.lib.database.connection.BaseDatabaseConnection;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
-/**
- * Represents a column of a database table which has a auto increment column as its primary key.
- * @see DatabaseColumn
- */
-public interface DatabaseColumnAutoIncrement<T> extends DatabaseColumn<T, Integer> {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@NoArgsConstructor
+public abstract class DatabaseColumnAutoIncrement<T> implements DatabaseColumn<T, Integer> {
+
+    private String table;
+
+    @Getter
+    @Accessors(fluent = true)
+    private int columnId;
+
+    protected DatabaseColumnAutoIncrement(String table) {
+        this.table = table;
+        this.columnId = -1;
+    }
+
+    protected abstract void createInDatabaseImpl(BaseDatabaseConnection connection);
 
     /**
-     * Fetch the id of the current class from the provided database using the {@link BaseDatabaseConnection} class.
+     * Creates a new column instance in the database via the provided database using the {@link BaseDatabaseConnection}
+     * class.
+     *
      * @param connection The provided database connection.
-     * @return the id if it was found in the database, a negative number (preferably -1) otherwise.
      */
-    public abstract int fetchId(BaseDatabaseConnection connection);
+    @Override
+    public final void createInDatabase(BaseDatabaseConnection connection) {
+        this.createInDatabaseImpl(connection);
+
+        try {
+            PreparedStatement statement = connection.get().prepareStatement("SELECT MAX(id) FROM %s".formatted(table));
+            ResultSet result = statement.executeQuery();
+
+            if (result.next()) {
+                this.columnId = result.getInt(1);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public boolean existsInDatabase(BaseDatabaseConnection connection) {
+        return this.columnId == -1;
+    }
+
+    public static String getIdsToString(List<? extends DatabaseColumnAutoIncrement<?>> list) {
+        return BaseDatabaseConnection.createStringFromList(
+                list.stream().map(DatabaseColumnAutoIncrement::columnId).toList(),
+                Object::toString
+        );
+    }
 
 }
