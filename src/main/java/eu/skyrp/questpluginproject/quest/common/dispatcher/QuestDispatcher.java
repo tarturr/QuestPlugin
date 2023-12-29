dbba69b991c751625c6f0c4a6920b9659383a7ae
tarturr <tartur.dev@gmail.com>
2023-12-29 11:56:47 +0100

    Initialized PropertyChangeSupport by default

--- src/main/java/eu/skyrp/questpluginproject/quest/common/dispatcher/QuestDispatcher.java
+++ src/main/java/eu/skyrp/questpluginproject/quest/common/dispatcher/QuestDispatcher.java
@@ -7,16 +7,21 @@ import eu.skyrp.questpluginproject.quest.WeeklyQuest;
 import eu.skyrp.questpluginproject.quest.common.Quest;
 import eu.skyrp.questpluginproject.quest.common.types.QuestType;
 
+import java.beans.PropertyChangeSupport;
+
 public class QuestDispatcher implements Dispatchable<Quest> {
 
     @Override
     public Quest dispatch(String str) {
-        return switch (QuestType.valueOf(str)) {
+        Quest quest = switch (QuestType.valueOf(str)) {
             case EVENT -> new EventQuest();
             case DAILY -> new DailyQuest();
             case ONLY_ONCE -> new OnlyOnceQuest();
             case WEEKLY -> new WeeklyQuest();
         };
+
+        quest.support(new PropertyChangeSupport(quest));
+        return quest;
     }
 
 }
